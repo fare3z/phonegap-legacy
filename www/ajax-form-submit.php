5deb27fca5f319c5b3387dfd51a416f9e100ad3f
Fareez Borhanudin <fare4z@gmail.com>
2024-10-07 08:07:28 +0800

    Update ajax-form-submit.php
    convert to mysqli

--- www/ajax-form-submit.php
+++ www/ajax-form-submit.php
@@ -1,5 +1,3 @@
-
-
 <?php
 
 $server = "mysql.main-hosting.com";
@@ -7,9 +5,13 @@ $username = "u102252830_root";
 $password = "123456";
 $database = "u102252830_sms";
 
-$con = mysql_connect($server, $username, $password) or die ("Could not connect: " . mysql_error());
+$con = mysqli_connect($server, $username, $password);
 
-mysql_select_db($database, $con);
+if (!$con) {
+    die("Could not connect: " . mysqli_connect_error());
+}
+
+mysqli_select_db($con, $database);
 
 $staffid=$_POST["staffid"];
 $pwd1=md5($_POST["pwd1"]);
@@ -22,34 +24,15 @@ $position=$_POST["position"];
 $state=$_POST["state"];
 
 
-$sql = "INSERT INTO tbl_user(staffid,pwd1,name,ic,position,address,state,email,peranan) ";
+$sql = "INSERT INTO tbl_user(staffid, pwd1, name, ic, position, address, state, email, peranan) ";
+$sql .= "VALUES ('$staffid', '$pwd1', '$name', '$ic', '$position', '$address', '$state', '$email', 'USER')";
 
-$sql .= "VALUES ('$staffid','$pwd1','$name','$ic','$position','$address','$state','$email','USER')";
-
-
-if (!mysql_query($sql, $con)) {
-	die('Error: ' . mysql_error());
+if (!mysqli_query($con, $sql)) {
+    die('Error: ' . mysqli_error($con));
 } else {
-	echo "Comment added";
+    echo "Comment added";
 }
 
-mysql_close($con);
+mysqli_close($con);
 
 ?>
-
-<!DOCTYPE html> 
-<html>
-<head>
-<meta charset="utf-8">
-<title>jQuery Mobile Web App</title>
-<link href="jquery.mobile.theme-1.0.min.css" rel="stylesheet" type="text/css"/>
-<link href="jquery.mobile.structure-1.0.min.css" rel="stylesheet" type="text/css"/>
-<script src="jquery-1.6.4.min.js" type="text/javascript"></script>
-<script src="jquery.mobile-1.0.min.js" type="text/javascript"></script>
-<script src="submit.js" type="text/javascript"></script>
-</head> 
-<body> 
-<div data-role="content">
-</div>
-</body>
-</html>
